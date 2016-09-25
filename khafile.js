a8db38dd1eaa4a187988478dfb740b58217de55e
patternspandemic <patternspandemic@live.com>
2016-09-25 16:21:51 -0700

    khafile addLibrary: polygonal-ds

--- khafile.js
+++ khafile.js
@@ -4,6 +4,7 @@ project.addAssets('Assets/**');
 project.addSources('Sources');
 project.addSources('Libraries/hxDelaunay/src');
 
+project.addLibrary('polygonal-ds');
 project.addLibrary('ecx');
 project.addLibrary('ecx_common');
 project.addLibrary('zui');
